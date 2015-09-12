92ffec12ba54cccbabb8ed706afc0011a7b63dc8
thejennywang <thejennywang@gmail.com>
2015-09-11 17:34:23 -0700

    set db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150811025109) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string   "name"
     t.text     "body"
     t.string   "url"
-    t.datetime "created_at"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
