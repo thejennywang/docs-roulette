4fbf1da609828995d799b637b854fb92858b801c
thejennywang <thejennywang@gmail.com>
2015-09-15 11:02:29 -0700

    updated secrets

--- Gemfile
+++ Gemfile
@@ -1,10 +1,9 @@
 source 'https://rubygems.org'
 
+ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
-
-ruby '2.0.0'
 
 gem 'pg'
 # Use SCSS for stylesheets
--- app/controllers/email_blasts_controller.rb
+++ app/controllers/email_blasts_controller.rb
@@ -1,10 +1,10 @@
 class EmailBlastsController < ApplicationController
   
   def create
-    @all_articles = Article.all
+    all_articles = Article.all
 
     Backerkat.all.each do |b|
-      BackerkatMailer.new_article(b, @all_articles.sample).deliver_now
+      BackerkatMailer.new_article(b, all_articles.sample).deliver_now
     end
 
     redirect_to root_url, notice: "Blast Sent!"
